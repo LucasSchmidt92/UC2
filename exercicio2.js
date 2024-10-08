e35be2dd1ff4b8fd2fabc02a745232e53be6fd88
LucasSchmidt92 <lucas_schmidt92@hotmail.com>
2024-10-07 21:55:44 -0300

    Add files via upload

---
+++ exercicio2.js
@@ -0,0 +1,24 @@
+let par1 = 4
+let par2 = 6
+let par3 = 8
+let par4 = 10
+let par5 = 12
+
+let impar1 = 3
+let impar2 = 5
+let impar3 = 7
+let impar4 = 9
+let impar5 = 11
+
+console.log (par1%2)
+console.log (par2%2)
+console.log (par3%2)
+console.log (par4%2)
+console.log (par5%2)
+console.log (impar1%2)
+console.log (impar2%2)
+console.log (impar3%2)
+console.log (impar4%2)
+console.log (impar5%2)
+
+console.log ("para descobrir se um numero é par,o resto da divisão dele por 2 deve ser 0. E, paradescobrir se um numero é impar, o resto da divisão dele por 2 deve ser 1")
