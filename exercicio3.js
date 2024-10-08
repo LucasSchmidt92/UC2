842b4da1f7a768d113834b83a4b39c8c14684322
LucasSchmidt92 <lucas_schmidt92@hotmail.com>
2024-10-08 13:01:04 -0300

    Delete exercicio3.js

--- exercicio3.js
+++
@@ -1,8 +0,0 @@
-let numero1 = 5
-let numero2 = 10
-let numero3 = 15
-
-console.log (numero1 > numero2) 
-console.log (numero2 < numero3)
-console.log (numero3 == numero1)
-console.log (numero1 <= numero3)
