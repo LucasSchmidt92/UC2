75d7b616037e7b0f1be2f251243d0b8a7bf8760f
LucasSchmidt92 <lucas_schmidt92@hotmail.com>
2024-10-07 21:59:34 -0300

    Add files via upload

---
+++ uc2 - algoritimos/07-10/comparadores.js
@@ -0,0 +1,42 @@
+let numero1 = 90
+let numero2 = 45
+
+//comparar se um é igual ao outro
+console.log (numero1 === numero2) // False
+
+
+
+
+
+
+
+//comparar se um é diferente ao outro
+console.log (numero1 !== numero2) // true
+
+
+
+//comparar se um é igual ao outro
+console.log (numero1 === numero2) // False
+
+
+
+//comparar se um NUMERO É maior que outro
+console.log (numero1 > numero2) // true
+
+
+
+//comparar se um é igual ou maior ao outro
+console.log (numero1 >= numero2) // true
+
+
+
+//comparar se um é menor ao outro
+console.log (numero1 < numero2) // False
+
+
+//comparar se um é igual ou menor
+console.log (numero1 <= numero2) // False
+
+//funcioina com string
+console.log("lucas === lucas")
+console.log("lucas !== lucas")
---
+++ uc2 - algoritimos/07-10/exercicio1.js
@@ -0,0 +1,9 @@
+//Criar 3 variaveis chamadas nota1, nota2, nota3
+// fazer media das tres notas
+
+let nota1 = 17
+let nota2 = 13
+let nota3 = 32
+
+let total = nota1 + nota2 + nota3
+console.log(total/3)
